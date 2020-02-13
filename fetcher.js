599a1469211bbdbdc53034551a60592b7743b34c
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-02-13 02:58:31 +0000

    added simple page downloader function

---
+++ fetcher.js
@@ -0,0 +1,37 @@
+const request = require('request');
+const fs = require('fs');3
+
+const inputArr = process.argv.slice(2); 
+const url = inputArr[0].toString();  
+const filePath = inputArr[1].toString();  // = .index.html
+
+
+// method 1: callback hell
+  // request (url, (error, response, body) => { 
+
+  //   console.log("writing to file")
+  //   fs.writeFile(filePath, response.body, (err) => {
+  //     fs.stat(filePath, (err, stat) => {
+  //       if (err) throw err;
+  //       console.log(`Downloaded and saved ${stat.size} bytes to ${filePath}`);
+  //     });
+  //   });
+  // });
+    
+
+// method 2: using writeFile as a callback to request function
+  const writer = (error, response, body) => { 
+
+   
+      console.log("writing to file")
+      fs.writeFile(filePath, response.body, (err) => {
+        fs.stat(filePath, (err, stat) => {
+          if (err) throw err;
+          console.log(`Downloaded and saved ${stat.size} bytes to ${filePath}`);
+        });
+      });
+    }
+
+    request (url, writer);
+
+
